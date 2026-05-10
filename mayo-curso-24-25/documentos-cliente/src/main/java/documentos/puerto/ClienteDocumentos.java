fa5cc592b4cd33724af641d8f567c1f9ec6f68e6
fjramirez1 <fj.ramirezlopez@um.es>
2026-05-10 13:54:21 +0200

    Fix @Query residual

--- mayo-curso-24-25/documentos-cliente/src/main/java/documentos/puerto/ClienteDocumentos.java
+++ mayo-curso-24-25/documentos-cliente/src/main/java/documentos/puerto/ClienteDocumentos.java
@@ -2,13 +2,12 @@ package documentos.puerto;
 
 import documentos.modelo.Documento;
 import documentos.rest.dto.NuevoDocumentoDTO;
-import retrofit2.http.Query;
 
 public interface ClienteDocumentos {
 
 	String createDocumento(NuevoDocumentoDTO nuevoDocumentoDTO) throws Exception;
 
-	Documento getDocumentoById(@Query("id") String id) throws Exception;
+	Documento getDocumentoById(String id) throws Exception;
 
-	void deleteDocumento(@Query("id") String id) throws Exception;
+	void deleteDocumento(String id) throws Exception;
 }
